9fe8e0f51609910be3b83bf985eb84021bfa72c4
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2023-03-10 12:40:32 +0200

    useState, useEffect hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { React, Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { Container, Chip, Text } from './App.styled';
 import { fetchGallery } from './fetch/fetch';
@@ -8,104 +8,192 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { Modal } from './Modal/Modal';
 import { Button } from './Button/Button';
 
-export class App extends Component {
-  state = {
-    imageGallery: [],
-    searchQuery: '',
-    isVisible: false,
-    isLoader: false,
-    page: 1,
-    isEmpty: false,
-    error: null,
-    largeUrl: null,
-    tag: null,
-  };
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.imgGalleryList(this.state.searchQuery, this.state.page);
-    }
-  }
-  imgGalleryList = async (searchQuery, page) => {
-    this.setState({ isLoader: true });
+export const App = () => {
+  const [imageGallery, setImageGallery] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const [isLoader, setIsLoader] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [error, setError] = useState(null);
+  const [largeUrl, setLargeUrl] = useState(null);
+  const [tag, setTag] = useState(null);
+
+  // useEffect(() => {
+  const imgGalleryList = async (searchQuery, page) => {
+    if (!searchQuery) return;
+    setIsLoader(true);
     try {
       const { hits, totalHits } = await fetchGallery(searchQuery, page);
-
       if (hits.length === 0) {
-        this.setState({ isEmpty: true });
+        setIsEmpty(true);
       }
-      this.setState(prev => ({
-        imageGallery: [...prev.imageGallery, ...hits],
-        isVisible: page < Math.ceil(totalHits / 12),
-      }));
+      setImageGallery(() => [...imageGallery, ...hits]);
+      setIsVisible(page < Math.ceil(totalHits / 12));
     } catch (error) {
-      this.setState({ error: error.message });
+      setError(error.message);
     } finally {
-      this.setState({ isLoader: false });
+      setIsLoader(false);
     }
   };
+  // }, []);
+  useEffect(() => {
+    if (setSearchQuery !== searchQuery || setPage !== page) {
+      imgGalleryList(searchQuery, page);
+    }
+  }, [searchQuery, page]);
 
-  handleSubmit = nameSearch => {
-    this.setState({
-      searchQuery: nameSearch,
-      imageGallery: [],
-      isVisible: false,
-      isLoader: false,
-      page: 1,
-      isEmpty: false,
-      error: null,
-    });
+  const loadMore = () => {
+    setPage(() => page + 1);
   };
 
-  loadMore = () => {
-    this.setState(prev => ({ page: prev.page + 1 }));
+  const onModalClose = () => {
+    setLargeUrl(null);
+    setTag(null);
   };
 
-  onModalClose = () => {
-    this.setState({ largeUrl: null, tag: null });
+  const openModal = (url, alt) => {
+    setLargeUrl(url);
+    setTag(alt);
+  };
+  const handleSubmit = nameSearch => {
+    setSearchQuery(nameSearch);
+    setImageGallery([]);
+    setIsVisible(false);
+    setIsLoader(false);
+    setPage(1);
+    setIsEmpty(false);
+    setError(null);
   };
 
-  openModal = (url, alt) => this.setState({ largeUrl: url, tag: alt });
+  return (
+    <Container>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          className: '',
+          duration: 1000,
+          style: {
+            background: '#ff0303',
+            color: '#fff',
+          },
+        }}
+      />
+      <Searchbar onSearch={handleSubmit} />
+      {searchQuery && (
+        <ImageGallery imageGallery={imageGallery} onOpenModal={openModal} />
+      )}
+      {largeUrl && <Modal url={largeUrl} alt={tag} onClose={onModalClose} />}
+      {isLoader && <Chip> {Spinner()} </Chip>}
 
-  render() {
-    const {
-      imageGallery,
-      isVisible,
-      searchQuery,
-      isLoader,
-      isEmpty,
-      error,
-      largeUrl,
-      tag,
-    } = this.state;
-    const { handleSubmit, loadMore, openModal, onModalClose } = this;
-    return (
-      <Container>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            className: '',
-            duration: 1000,
-            style: {
-              background: '#ff0303',
-              color: '#fff',
-            },
-          }}
-        />
-        <Searchbar onSearch={handleSubmit} />
-        {searchQuery && (
-          <ImageGallery imageGallery={imageGallery} onOpenModal={openModal} />
-        )}
-        {largeUrl && <Modal url={largeUrl} alt={tag} onClose={onModalClose} />}
-        {isLoader && <Chip> {Spinner()} </Chip>}
+      {isVisible && <Button loadMore={loadMore} />}
 
-        {isVisible && <Button loadMore={loadMore} />}
+      {isEmpty && <Text> Sorry. There are no images ... </Text>}
+      {error && <Text> Something went wrong. Try again later.😭 </Text>}
+    </Container>
+  );
+};
 
-        {isEmpty && <Text> Sorry. There are no images ... </Text>}
-        {error && <Text> Something went wrong. Try again later.😭 </Text>}
-      </Container>
-    );
-  }
-}
+//===============================
+
+// export class App extends Component {
+//   state = {
+//     imageGallery: [],
+//     searchQuery: '',
+//     isVisible: false,
+//     isLoader: false,
+//     page: 1,
+//     isEmpty: false,
+//     error: null,
+//     largeUrl: null,
+//     tag: null,
+//   };
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.searchQuery !== this.state.searchQuery ||
+//       prevState.page !== this.state.page
+//     ) {
+//       this.imgGalleryList(this.state.searchQuery, this.state.page);
+//     }
+//   }
+//   imgGalleryList = async (searchQuery, page) => {
+//     this.setState({ isLoader: true });
+//     try {
+//       const { hits, totalHits } = await fetchGallery(searchQuery, page);
+
+//       if (hits.length === 0) {
+//         this.setState({ isEmpty: true });
+//       }
+//       this.setState(prev => ({
+//         imageGallery: [...prev.imageGallery, ...hits],
+//         isVisible: page < Math.ceil(totalHits / 12),
+//       }));
+//     } catch (error) {
+//       this.setState({ error: error.message });
+//     } finally {
+//       this.setState({ isLoader: false });
+//     }
+//   };
+
+//   handleSubmit = nameSearch => {
+//     this.setState({
+//       searchQuery: nameSearch,
+//       imageGallery: [],
+//       isVisible: false,
+//       isLoader: false,
+//       page: 1,
+//       isEmpty: false,
+//       error: null,
+//     });
+//   };
+
+//   loadMore = () => {
+//     this.setState(prev => ({ page: prev.page + 1 }));
+//   };
+
+//   onModalClose = () => {
+//     this.setState({ largeUrl: null, tag: null });
+//   };
+
+//   openModal = (url, alt) => this.setState({ largeUrl: url, tag: alt });
+
+//   render() {
+//     const {
+//       imageGallery,
+//       isVisible,
+//       searchQuery,
+//       isLoader,
+//       isEmpty,
+//       error,
+//       largeUrl,
+//       tag,
+//     } = this.state;
+//     const { handleSubmit, loadMore, openModal, onModalClose } = this;
+//     return (
+//       <Container>
+//         <Toaster
+//           position="top-right"
+//           toastOptions={{
+//             className: '',
+//             duration: 1000,
+//             style: {
+//               background: '#ff0303',
+//               color: '#fff',
+//             },
+//           }}
+//         />
+//         <Searchbar onSearch={handleSubmit} />
+//         {searchQuery && (
+//           <ImageGallery imageGallery={imageGallery} onOpenModal={openModal} />
+//         )}
+//         {largeUrl && <Modal url={largeUrl} alt={tag} onClose={onModalClose} />}
+//         {isLoader && <Chip> {Spinner()} </Chip>}
+
+//         {isVisible && <Button loadMore={loadMore} />}
+
+//         {isEmpty && <Text> Sorry. There are no images ... </Text>}
+//         {error && <Text> Something went wrong. Try again later.😭 </Text>}
+//       </Container>
+//     );
+//   }
+// }
--- src/components/fetch/fetch.jsx
+++ src/components/fetch/fetch.jsx
@@ -5,8 +5,16 @@ const API_KEY = '32552972-dc07813a8434780c4e0dd03fe';
 
 export const fetchGallery = async (searchQuery, page) => {
   const { data } = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&image_type=photo&orientation=horizontal&per_page=12`
+    `${BASE_URL}?image_type=photo&orientation=horizontal&per_page=12`,
+    {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        page: page,
+      },
+    }
   );
+  //
 
   return data;
 };
